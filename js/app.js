f28f7f150480cff5c340df5fb4c65fa73af8099c
EC2 Default User <ec2-user@ip-172-31-23-140.us-east-2.compute.internal>
2021-11-05 13:49:52 +0000

    ３．レイアウト整え

--- js/app.js
+++ js/app.js
@@ -31,22 +31,14 @@ window.addEventListener("DOMContentLoaded", () => {
   //入力欄にplayer_typeという名前をつけた
   const player_type = document.getElementById("your_type");
   
-  
-  /*
-  //答えは天智天皇だよ
-  const answers = ["卑弥呼","聖徳太子","蘇我馬子","小野妹子","中臣鎌足","天智天皇"];
-  //問題文を登録
-  const items = ["お告げ聞く","聖徳太子text","蘇我馬子text","小野妹子text","中臣鎌足text","天智天皇text"];
-  */
-  
-  //問題を多次元配列で登録　箱：items　答え：answer　問題：text
+  //問題を多次元配列で登録　箱：items　答え：answer　問題：text　イラスト：image
   let items = [
-    { answer: "卑弥呼" , text1: "お告げ聞く" , text2: "邪馬台国の" , text3: "女王さま"},
-    { answer: "聖徳太子" , text1: "能力で" , text2: "与える冠位" , text3: "十二階"},
-    { answer: "蘇我馬子" , text1: "豪族で" , text2: "推古天皇の" , text3: "おじさんよ" },
-    { answer: "小野妹子" , text1: "遣隋使" , text2: "聖徳太子に" , text3: "命じられ" },
-    { answer: "中臣鎌足" , text1: "死ぬ直前" , text2: "藤原の姓を" , text3: "与えられ" },
-    { answer: "天智天皇" , text1: "壬申の乱" , text2: "甥に勝って" , text3: "天皇に" }
+    { answer: "卑弥呼" , text1: "お告げ聞く" , text2: "邪馬台国の" , text3: "女王さま" , image:"../image/01.png"},
+    { answer: "聖徳太子" , text1: "能力で" , text2: "与える冠位" , text3: "十二階" , image:"../image/02.png"},
+    { answer: "蘇我馬子" , text1: "豪族で" , text2: "推古天皇の" , text3: "おじさんよ"  , image:"../image/03.jpg"},
+    { answer: "小野妹子" , text1: "遣隋使" , text2: "聖徳太子に" , text3: "命じられ"  , image:"../image/04.png"},
+    { answer: "中臣鎌足" , text1: "死ぬ直前" , text2: "藤原の姓を" , text3: "与えられ"  , image:"../image/05.png"},
+    { answer: "天智天皇" , text1: "壬申の乱" , text2: "甥に勝って" , text3: "天皇に"  , image:"../image/06.png"}
     ];
     
   // 配列をシャッフル
@@ -68,10 +60,12 @@ window.addEventListener("DOMContentLoaded", () => {
   text2.textContent = items[0].text2;
   const text3 = document.getElementById("text3");
   text3.textContent = items[0].text3;
+  const image = document.getElementById("image");
+  image.src = items[0].image;
     
 //制限時間
 const timer = document.getElementById("timer");
-let TIME = 200;
+let TIME = 20;
 
 const countdown = setInterval(function(){
   
@@ -80,19 +74,27 @@ if(TIME <= 0) finish();
   
 }, 1000);
 
+let count = 0;
+let state = true;
+
 function finish(){
   clearInterval(countdown);
-  items[ item_no].answer.textContent = '正解数は' + count + '個でした！';
+  
+//  const score = document.getElementById("text2");
+  text2.textContent = '正解数は' + count  + '個でした！';
+  text1.textContent = "";
+  text3.textContent = "";
+  state = false;
 }
 
-var i = -1 ;
+var i = 0 ;
 
   //判定ボタンをクリックしたときのイベントを定義。無名関数を設定
   judge_button.addEventListener("click", function(){
   //let はブロックスコープの変数定義。入力値にinputと名前をつけた
     let input = player_type.value;
     
-    i = i+1 ;
+    if(!state) return; //終了後に操作できないようにする
       
     let answer = items[i].answer;
     
@@ -100,26 +102,30 @@ var i = -1 ;
     //入力値が答えと同じだったら正解、違ったら不正解をコンソールログに出力
     if(input == answer){
       console.log("正解");
+      count = count + 1 ;
+      i = i+1 ;
+      
+    //問題文を表示
+    const text1 = document.getElementById("text1");
+    text1.textContent = items[i].text1;
+    const text2 = document.getElementById("text2");
+    text2.textContent = items[i].text2;
+    const text3 = document.getElementById("text3");
+    text3.textContent = items[i].text3;
+    
+    const image = document.getElementById("image");
+    image.src = items[i].image;
+    
     } else {
       console.log("不正解");
     }
-    
-    
-  //問題文を表示
-  const text1 = document.getElementById("text1");
-  text1.textContent = items[i+1].text1;
-  const text2 = document.getElementById("text2");
-  text2.textContent = items[i+1].text2;
-  const text3 = document.getElementById("text3");
-  text3.textContent = items[i+1].text3;
+  
+    player_type.value = "";
+    input.focus();
     
   
   })
 })
-
-
-
-
 
 
 
